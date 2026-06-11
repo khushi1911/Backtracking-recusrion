16d182809bbc23c695620dd1827f5296caa3f708
khushi1911 <khushikaushik2030@gmail.com>
2026-06-12 00:16:06 +0530

    to use backtracking for permutation

---
+++ permutation.java
@@ -0,0 +1,19 @@
+public class permutation {
+
+    public static void permut(String str , String per , int idx){
+
+        if(str.length()==0){
+            System.out.println(per);
+            return;
+        }
+        for (int i=0; i<str.length(); i++){
+            char curr = str.charAt(i);
+            String newstr = str.substring(0,i) + str.substring(i+1);
+            permut(newstr, per+curr, idx+1);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "ABC";
+        permut(str ,"",0);
+    }
+}
